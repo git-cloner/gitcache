629c8c7633bb5c9b0044503920cb3ff3fb735f58
little51 <little51@126.com>
2020-08-08 17:06:01 +0800

    fixed remote has .git hitcount issue

--- gitcache.go
+++ gitcache.go
@@ -394,7 +394,7 @@ func RequestHandler(basedir string) http.HandlerFunc {
 				w.Header().Set("Content-Type", fmt.Sprintf("application/x-%s-result", httpParams.Gitservice))
 				w.WriteHeader(200)
 				execShelldPipe(httpParams.Gitservice, []string{"--stateless-rpc", local}, w, r)
-				go AddHitCount(remote)
+				go AddHitCount(strings.Replace(remote, ".git", "", -1))
 			} else {
 				hdrNocache(w)
 				//redirect to github.com clone
