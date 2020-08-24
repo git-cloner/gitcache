936d09ea224f30c9661651ae30c3708b63b7a375
little51 <little51@126.com>
2020-08-24 20:21:43 +0800

    use port to check gitcache mode or download mode

--- main.go
+++ main.go
@@ -18,10 +18,13 @@ func main() {
 	flag.StringVar(&port, "p", "5000", "default port:5000")
 	flag.Parse()
 	log.Printf("cache basedir:%v , port:%v", g_Basedir, port)
-	//connect to db
-	InitDb()
-	//cron
-	Cron()
+	//port == 5000 gitcacher , posrt != 5000 downloader
+	if port == "5000" {
+		//connect to db
+		InitDb()
+		//cron
+		Cron()
+	}
 	//listen
 	http.HandleFunc("/", RequestHandler(g_Basedir))
 	address := ":" + port
