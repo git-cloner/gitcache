b3b1b2e48de6e063ae1111492a73388978bd2410
little51 <little51@126.com>
2020-08-29 07:20:47 +0800

    add cache support

--- plus/plus.go
+++ plus/plus.go
@@ -4,11 +4,39 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
+type HttpParams struct {
+	Repository string
+	Gitservice string
+	IsInfoReq  bool
+}
+
+func parseHttpParams(realurl string) HttpParams {
+	u, err := url.Parse(realurl)
+	if err != nil {
+		panic(err)
+	}
+	str := strings.Split(u.Path, "/")
+	if len(str) < 4 {
+		panic("bad request params")
+	}
+	_Repository := str[1] + "/" + str[2] + "/" + str[3]
+	var _Gitservice = strings.Replace(u.RawQuery, "service=", "", -1)
+	if _Gitservice == "" {
+		if (strings.Index(str[4], "git") != -1) && (strings.Index(str[4], "pack") != -1) {
+			_Gitservice = str[4]
+		}
+	}
+	_IsInfoReq := (str[4] == "info")
+	var httpParams HttpParams = HttpParams{Repository: _Repository, Gitservice: _Gitservice, IsInfoReq: _IsInfoReq}
+	return httpParams
+}
+
 func rinetGitRequest(w http.ResponseWriter, r *http.Request, url string) {
-	log.Printf("redirect to github.com : %v,%v\n", url, r.Method)
+	log.Printf("redirect to : %v,%v\n", url, r.Method)
 	client := &http.Client{}
 	req, err := http.NewRequest(r.Method, url, r.Body)
 	for k, v := range r.Header {
@@ -29,7 +57,7 @@ func rinetGitRequest(w http.ResponseWriter, r *http.Request, url string) {
 	//
 	flusher, ok := w.(http.Flusher)
 	if !ok {
-		panic("redirect to github.com to be an http.Flusher")
+		panic("redirect to be an http.Flusher")
 	}
 	p := make([]byte, 20480)
 	for {
@@ -38,10 +66,10 @@ func rinetGitRequest(w http.ResponseWriter, r *http.Request, url string) {
 		if n_read > 0 {
 			n_write, err := w.Write(p[:n_read])
 			if err != nil {
-				panic("redirect to github.com with pipe error:" + err.Error())
+				panic("redirect to with pipe error:" + err.Error())
 			}
 			if n_read != n_write {
-				panic("redirect to github.com with pipe failed to write data")
+				panic("redirect to with pipe failed to write data")
 			}
 			flusher.Flush()
 		}
@@ -88,10 +116,20 @@ func preProcUrl(url string) string {
 	}
 	log.Printf("real url: %s,token: %s\n", realurl, token)
 	if ValidUser(token) {
-		return realurl
+		return "/" + realurl
 	} else {
 		return ""
 	}
+}
+
+func imageExists(url string) bool {
+	resp, err := http.Get("https://gitclone.com/gitcache/image/" + url)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusOK
 }
 
 func RequestHandler() http.HandlerFunc {
@@ -100,17 +138,27 @@ func RequestHandler() http.HandlerFunc {
 			DownloadFile(w, r)
 			return
 		}
-		var url = preProcUrl(r.URL.RequestURI())
-		if url == "" {
+		var realurl = preProcUrl(r.URL.RequestURI())
+		if realurl == "" {
 			log.Printf("unknown token : %v\n", r.URL.RequestURI())
 			w.WriteHeader(404)
 			return
 		}
-		url = "https://" + url
-		log.Printf("client send git request: %s\n", url)
+		var httpParams HttpParams = parseHttpParams(realurl)
+		repos := httpParams.Repository
+		if strings.HasSuffix(repos, ".git") {
+			repos = repos[:len(repos)-4]
+		}
+		useImage := imageExists(repos)
+		log.Printf("check image exists: %s %v\n", repos, useImage)
+		realurl = "https:/" + realurl
+		//redirect to github.com or cache to clone
+		if useImage {
+			realurl = strings.Replace(realurl, "https://github.com", "http://gitclonecache.com:22003/github.com", -1)
+		}
+		log.Printf("client send : %s\n", realurl)
 		hdrNocache(w)
-		//redirect to github.com clone
-		rinetGitRequest(w, r, url)
+		rinetGitRequest(w, r, realurl)
 		return
 	}
 }
